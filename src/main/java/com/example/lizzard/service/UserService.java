74594106f6263c8fec23aacf45bb24566b564b00
Anderson Camilo Vargas <52604167+AndersonVargas8@users.noreply.github.com>
2022-02-24 20:00:26 -0500

    Delete src/main/java/com/example/lizzard/service directory

--- src/main/java/com/example/lizzard/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.lizzard.service;
-
-import com.example.lizzard.model.User;
-import com.example.lizzard.web.dto.UserRegistrationDto;
-
-public interface UserService {
-    User save(UserRegistrationDto registrationDto);
-
-}
