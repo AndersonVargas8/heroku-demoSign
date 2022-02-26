5d83b632b03833a1ae056ed3f7189ffe06c72462
AndersonVargas8 <anvargasa@unal.edu.co>
2022-02-25 19:49:49 -0500

    better

--- src/main/java/com/example/lizzard/IndexController.java
+++ src/main/java/com/example/lizzard/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.lizzard;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping("/")
     public String index() {
-        return "Hola mundo.";
+        return "Hola mundo";
     }
 }
